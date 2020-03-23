801cb7f1a1dd0d02a7899380246464ed7e6b75a5
realjanpaulus <janpaulus95@web.de>
2020-03-23 18:07:40 +0100

    refactor: hide code cell function

--- src/utils.py
+++ src/utils.py
@@ -18,3 +18,23 @@ def css_styling():
 	return HTML(styles)
 
 
+
+def hide_code():
+	"""Hide jupyter notebook code cells."""
+	toggle_code_str = """
+	<form action="javascript:code_toggle()"><input type="submit" id="toggleButton" value="Show/Hide Code Cell"></form>
+	"""
+
+	toggle_code_prepare_str = """
+    <script>
+    function code_toggle() {
+        if ($('div.cell.code_cell.rendered.selected div.input').css('display')!='none'){
+            $('div.cell.code_cell.rendered.selected div.input').hide();
+        } else {
+            $('div.cell.code_cell.rendered.selected div.input').show();
+        }
+    }
+    </script>"""
+	display(HTML(toggle_code_prepare_str + toggle_code_str))
+
+
